5b9496e5c49a8e88f08cc9e28379bcdc5fd180ee
Kairav Parekh <kparekh01@gmail.com>
2016-09-15 21:01:16 -0400

    rendered json as pdf

--- app/controllers/resumes_controller.rb
+++ app/controllers/resumes_controller.rb
@@ -1,4 +1,14 @@
 class ResumesController < ApplicationController
+
   def show
-  end
-end
+  @text = Unirest.get("https://data.cityofnewyork.us/resource/byk8-bdfw.json").body
+      respond_to do |format|
+        format.html
+            format.pdf do
+              pdf = Prawn::Document.new
+              pdf.text "#{@text[1]["borough"]}"
+              send_data pdf.render, type: "application/pdf", disposition: "inline"
+            end
+          end
+        end
+    end

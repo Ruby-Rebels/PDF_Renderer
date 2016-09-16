8075bcbb7de2808d6cd4f3e770b131a58bfd6ee9
Justin Hernandez <kaspersky102@aol.com>
2016-09-15 21:32:47 -0400

    made a call to students api

--- app/controllers/resumes_controller.rb
+++ app/controllers/resumes_controller.rb
@@ -1,12 +1,12 @@
 class ResumesController < ApplicationController
 
   def show
-  @text = Unirest.get("https://data.cityofnewyork.us/resource/byk8-bdfw.json").body
+  @text = Unirest.get("http://localhost:3000/api/v1/students.json").body
       respond_to do |format|
         format.html
             format.pdf do
               pdf = Prawn::Document.new
-              pdf.text "#{@text[1]["borough"]}"
+              pdf.text "#{@text[0]['']}"
               send_data pdf.render, type: "application/pdf", disposition: "inline"
             end
           end
